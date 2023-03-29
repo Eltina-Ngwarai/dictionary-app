619587976cf4b5c96b12118844a8f4c05d786dc2
elty-annie <eltinangwarai@gmail.com>
2023-03-29 13:57:13 +0200

    added the basic search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
     <div className="App">
-      <div>Hi there</div>
+      <div className='container'>
+        <main>
+          <Dictionary/>
+        </main>
+
+      </div>
       
     </div>
   );
